69226169e4b124bd0d0033c60238b1dc912776a5
Lan Phan <lanc_phan@hotmail.com>
2017-11-13 14:18:00 -0500

    Change the status text when the document is ready

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
 
+document.addEventListener("DOMContentLoaded",function(){
+  const body = document.querySelector('.light');
+  const text = document.querySelector('.status');
+  const lightSwitch = document.querySelector('.switch');
 
+  window.addEventListener("load",function(){
+    text.innerText = "Click the light switch";
+  })
+});
