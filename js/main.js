70a4f9b3df0a96ff443adf2cb853d1b14d67810b
Lan Phan <lanc_phan@hotmail.com>
2017-11-13 14:32:31 -0500

    create the light switch for on and off

--- js/main.js
+++ js/main.js
@@ -1,10 +1,32 @@
-
+// select and store important selector into constant variable
 document.addEventListener("DOMContentLoaded",function(){
   const body = document.querySelector('.light');
   const text = document.querySelector('.status');
   const lightSwitch = document.querySelector('.switch');
 
+  // instruction
   window.addEventListener("load",function(){
     text.innerText = "Click the light switch";
   })
+
+  lightSwitch.addEventListener('click', function(){
+
+    // turn off
+    if (lightSwitch.classList.contains('on')) {
+      lightSwitch.classList.remove('on');
+      lightSwitch.classList.add('off');
+      body.style.backgroundColor = '#000';
+      text.style.color = '#fff'
+      text.innerText = "It's so dark in here!";
+    } else {
+
+      // turn on
+      lightSwitch.classList.add('on');
+      lightSwitch.classList.remove('off');
+      body.style.backgroundColor = '#fff';
+      text.style.color = '#000';
+      text.innerText = "It's so bright in here!";
+    }
+  });
+
 });
